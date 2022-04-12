33dd4c6478736c4e76c24c9c209a5f31ec347445
Clement <c.legay@autobilan-systems.fr>
2022-04-12 16:31:10 +0200

    base

---
+++ src/components/HeaderBackground.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="header">
+    <div class="header_logo">
+      <v-img width="200" height="50" src="../assets/images/textLogo.png"></v-img>
+    </div>
+    <div class="header_nav">
+      <ul>
+        <li v-for="(item, index) in navigation" :key="index">{{item}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "background",
+  data () {
+    return {
+      navigation: [
+        "Sorts",
+        "Talismans",
+        "Armures",
+        "Loots",
+        "Mobs",
+        "Boss",
+        "Quêtes"
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
+  }
+</style>
